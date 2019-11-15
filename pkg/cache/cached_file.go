9b665d6d9a7af0b47d32fed0d027b9fe804494af
Langhammer, Jens <jens.langhammer@haufe-lexware.com>
2019-11-15 14:01:52 +0100

    cache file's stat() data, accept range requests with cache enabled

--- pkg/cache/cached_file.go
+++ pkg/cache/cached_file.go
@@ -1,6 +1,8 @@
 package cache
 
 import (
+	"bytes"
+	"encoding/json"
 	"io/ioutil"
 	"net/http"
 	"os"
@@ -19,15 +21,26 @@ type CachedFile struct {
 
 // Serve Write cached contents to http response
 func (cf CachedFile) Serve(w http.ResponseWriter, r *http.Request) {
-	err := cf.readFileIfNeeded()
+	p, err := cf.readFileIfNeeded()
 	if err != nil {
 		cf.FS.Logger.Debug(err)
 	}
-	p, err := cf.FS.Cache.Get(cf.Key)
-	if err != nil {
-		cf.FS.Logger.Debug(err)
+	re := bytes.NewReader(p)
+	statByte, err := cf.FS.Cache.Get("stat-" + cf.Key)
+	if err == bigcache.ErrEntryNotFound {
+		statByte, err = cf.readStatsIntoCache()
+		if err != nil {
+			cf.FS.Logger.Warning(err)
+		}
+	} else if err != nil {
+		cf.FS.Logger.Warning(err)
 	}
-	w.Write(p)
+	var stats Stat
+	err = json.Unmarshal(statByte, &stats)
+	if err != nil {
+		cf.FS.Logger.Warning(err)
+	}
+	http.ServeContent(w, r, stats.Name, stats.ModTime, re)
 }
 
 // Readdir Wrapper around fs.File.Readdir
@@ -40,22 +53,58 @@ func (cf CachedFile) Stat() (os.FileInfo, error) {
 	return cf.File.Stat()
 }
 
-func (cf CachedFile) readFileIfNeeded() error {
-	_, err := cf.FS.Cache.Get(cf.Key)
+func (cf CachedFile) readFileIfNeeded() ([]byte, error) {
+	data, err := cf.FS.Cache.Get(cf.Key)
 	if err == bigcache.ErrEntryNotFound {
 		cf.FS.Logger.Debug("File not in Cache, reading from disk")
-		buffer, err := ioutil.ReadAll(cf.File)
+		data, err := cf.readIntoCache()
 		if err != nil {
-			cf.FS.Logger.Warningf("ReadAll Err: %s", err)
-			return err
+			cf.FS.Logger.Warning(err)
 		}
-		err = cf.FS.Cache.Set(cf.Key, buffer)
+		// We also cache the stats of the file
+		_, err = cf.readStatsIntoCache()
 		if err != nil {
-			cf.FS.Logger.Warningf("CacheSet: %s", err)
-			return err
+			cf.FS.Logger.Warning(err)
 		}
+		return data, nil
 	} else if err != nil {
 		cf.FS.Logger.Warningf("CacheGet: %s", err)
 	}
-	return nil
+	return data, nil
+}
+
+func (cf CachedFile) readIntoCache() ([]byte, error) {
+	buffer, err := ioutil.ReadAll(cf.File)
+	if err != nil {
+		cf.FS.Logger.Warningf("ReadAll Err: %s", err)
+		return nil, err
+	}
+	err = cf.FS.Cache.Set(cf.Key, buffer)
+	if err != nil {
+		cf.FS.Logger.Warningf("CacheSet: %s", err)
+		return nil, err
+	}
+	return buffer, nil
+}
+
+func (cf CachedFile) readStatsIntoCache() ([]byte, error) {
+	stats, err := cf.File.Stat()
+	if err != nil {
+		cf.FS.Logger.Warningf("Stat Err: %s", err)
+		return nil, err
+	}
+	marshalled, err := json.Marshal(Stat{
+		Name:    stats.Name(),
+		ModTime: stats.ModTime(),
+	})
+	if err != nil {
+		cf.FS.Logger.Warningf("JSON Marshal: %s", err)
+		return marshalled, err
+	}
+	err = cf.FS.Cache.Set("stat-"+cf.Key, marshalled)
+	if err != nil {
+		cf.FS.Logger.Warningf("CacheSet: %s", err)
+		return marshalled, err
+	}
+	return marshalled, nil
 }
---
+++ pkg/cache/cached_stat.go
@@ -0,0 +1,10 @@
+package cache
+
+import (
+	"time"
+)
+
+type Stat struct {
+	Name    string
+	ModTime time.Time
+}
